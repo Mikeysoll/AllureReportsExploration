6bf29f77dff0a1bc33e9656ee0851a08f04b5b9a
Mikhail Solovyov <solovev02@yandex.ru>
2025-11-03 15:11:50 +0300

    minor changes

--- src/test/java/base/TestBase.java
+++ src/test/java/base/TestBase.java
@@ -4,16 +4,22 @@ import com.codeborne.selenide.Configuration;
 import com.codeborne.selenide.logevents.SelenideLogger;
 import io.qameta.allure.selenide.AllureSelenide;
 import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 
 public class TestBase {
 
     @BeforeAll
-    static void beforeAll() {
+    static void configSelenide() {
         Configuration.baseUrl = "https://github.com/";
         Configuration.browserSize = "1920x1080";
         Configuration.pageLoadStrategy = "eager";
+        Configuration.holdBrowserOpen = false;
+    }
+
+    @BeforeEach
+    void allureListener() {
         SelenideLogger.addListener("allure", new AllureSelenide());
-        Configuration.holdBrowserOpen = true;
+
     }
 
 }
