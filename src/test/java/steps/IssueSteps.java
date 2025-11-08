800bdb67bc14a75e41c0e09e5a1db48cbecce5fe
Mikhail Solovyov <solovev02@yandex.ru>
2025-11-08 12:19:29 +0300

    added IssuePage + refactor IssueSteps

--- src/test/java/steps/IssueSteps.java
+++ src/test/java/steps/IssueSteps.java
@@ -1,37 +1,34 @@
 package steps;
 
-import com.codeborne.selenide.Condition;
 import io.qameta.allure.Step;
-
-import static com.codeborne.selenide.Selectors.byText;
-import static com.codeborne.selenide.Selenide.$;
-import static com.codeborne.selenide.Selenide.open;
+import pages.IssuePage;
 
 public class IssueSteps {
 
-    @Step("Opening github profile")
+    private final IssuePage issuePage = new IssuePage();
+
+    @Step("Opening GitHub profile")
     public void openGitProfile(String profileName) {
-        open(profileName);
+        issuePage.openProfile(profileName);
     }
 
     @Step("Opening repository tab")
     public void openRepositoryTab() {
-        $("[data-tab-item=\"repositories\"]").click();
+        issuePage.openRepositoryTab();
     }
 
     @Step("Opening repository page")
     public void openRepositoryPage() {
-        $(byText("AllureReportsExploration")).click();
+        issuePage.openRepositoryPage();
     }
 
     @Step("Opening issue tab")
     public void openIssueTab() {
-        $("#issues-tab").click();
+        issuePage.openIssueTab();
     }
 
-    @Step("Checking issue title")
+    @Step("Checking issue title: {issueTitle}")
     public void checkIssueTitle(String issueTitle) {
-        $("[data-testid=\"issue-pr-title-link\"]")
-                .shouldHave(Condition.text(issueTitle));
+        issuePage.checkIssueTitle(issueTitle);
     }
 }
